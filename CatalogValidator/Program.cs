11c2cea17eabfa855281f9fe2dfd4adbd81a68f7
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2025-11-29 17:43:21 +0100

    CatalogValidator: Add new catalog validator tool

---
+++ CatalogValidator/Program.cs
@@ -0,0 +1,12 @@
+﻿using Iros;
+using Iros.Workshop;
+
+class Program {
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Validating: {args[0]}");
+        Util.Deserialize<Catalog>(args[0]);
+        Console.WriteLine("Validation completed successfully.");
+    }
+}
